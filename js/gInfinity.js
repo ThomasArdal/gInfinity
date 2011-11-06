dcdc857d9d641041186d902742fd0404e0e446d8
tardal <tardal@ebay.com>
2011-11-06 20:21:02 +0100

    Now able to load more than 10 pages.

--- js/gInfinity.js
+++ js/gInfinity.js
@@ -1,24 +1,43 @@
-$(document).ready(function () {
-    if (document.location.href.indexOf('://www.google.') == -1) return;
-    var loadcounter = 0;
+function execute() {
     alreadyloading = false;
+    var nextAnchor = $('#pnnext');
+    if (!nextAnchor) return;
+    var href = nextAnchor.attr('href');
     $(window).scroll(function () {
         var rso = $('#rso');
         if (!rso) return;
         if (($(window).scrollTop() + $(window).height()) >= ($('body').height() * 0.9)) {
             if (alreadyloading == false) {
                 alreadyloading = true;
-                var td = $('td[class=cur]');
-                var href = $(td.siblings().get(loadcounter + 1)).find('a:first-child').attr('href');
                 var parts = window.location.href.split('/');
-                if (parts.length < 3) return;
-                $.get(href, function (data) {
-                    var toAppend = $(data).find('#rso').html();
-                    rso.append(toAppend);
+                if (parts.length < 3) {
                     alreadyloading = false;
-                    loadcounter++;
-                });
+                    return;
+                }
+
+                if (href) {
+                    $.get(href, function (data) {
+                        var toAppend = $(data).find('#rso').html();
+                        nextAnchor = $(data).find('#pnnext');
+                        if (!nextAnchor) {
+                            href = null;
+                        } else {
+                            href = nextAnchor.attr('href');
+                        }
+
+                        rso.append(toAppend);
+                        alreadyloading = false;
+                    });
+                } else {
+                    alreadyloading = false;
+                }
             }
         }
     });
+}
+
+$(document).ready(function () {
+    if (document.location.href.indexOf('://www.google.') == -1) return;
+    // Wait for Google to load the page
+    setTimeout('execute()', 300);
 });
