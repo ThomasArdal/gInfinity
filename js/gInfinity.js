5b206f49e298cc021067d2ce12df8e50a1cb79e4
tardal <tardal@ebay.com>
2011-11-06 19:39:54 +0100

    Implemented extension for all localized versions of google.

--- js/gInfinity.js
+++ js/gInfinity.js
@@ -1,17 +1,18 @@
-//TODO: support more than 10 pages
 $(document).ready(function () {
+    if (document.location.href.indexOf('://www.google.') == -1) return;
     var loadcounter = 0;
     alreadyloading = false;
     $(window).scroll(function () {
         var rso = $('#rso');
         if (!rso) return;
-        //alert(($(window).scrollTop() + $(window).height()) >= ($('body').height() * 0.9));
         if (($(window).scrollTop() + $(window).height()) >= ($('body').height() * 0.9)) {
             if (alreadyloading == false) {
                 alreadyloading = true;
                 var td = $('td[class=cur]');
                 var href = $(td.siblings().get(loadcounter + 1)).find('a:first-child').attr('href');
-                $.get('https://www.google.com' + href, function (data) {
+                var parts = window.location.href.split('/');
+                if (parts.length < 3) return;
+                $.get(href, function (data) {
                     var toAppend = $(data).find('#rso').html();
                     rso.append(toAppend);
                     alreadyloading = false;
